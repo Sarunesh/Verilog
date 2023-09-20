bc073e6e09c82ef83028f911fff13ff94d1113c6
Sarunesh S <118706760+Sarunesh@users.noreply.github.com>
2023-09-20 21:40:28 +0530

    Update 32bit_adder.v

--- 32bit_adder/32bit_adder.v
+++ 32bit_adder/32bit_adder.v
@@ -1,3 +1,10 @@
+module add16 ( input[15:0] a, input[15:0] b, input cin, output[15:0] sum, output cout );
+    always@*
+        begin
+            sum=a^b^cin;
+            cout=(a&b) | (b&cin) | (cin&a);
+        end
+endmodule
 module top_module(input [31:0] a, b, output [31:0] sum);
     wire w1, w2;
     add16 LB(a[15:0], b[15:0], 0, sum[15:0], w1), HB(a[31:16], b[31:16], w1, sum[31:16], w2);
